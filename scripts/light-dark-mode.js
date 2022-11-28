d2d82262fdfabae4985e8fac3cf0b147fdf45edb
tom-2810 <tom.poortman@hva.nl>
2022-11-28 15:23:36 +0100

    summary volgorde

--- scripts/light-dark-mode.js
+++ scripts/light-dark-mode.js
@@ -1,6 +1,6 @@
 let modeSwitch = document.getElementById("mode");
 
-document.getElementById("mode").addEventListener('click', function () {
+modeSwitch.addEventListener('click', function () {
     modeSwitch.checked ? 
     document.documentElement.setAttribute("data-theme", "dark") : 
     document.documentElement.setAttribute("data-theme", "");
