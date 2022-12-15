ca86567e7bbd087b5160d7415d28928e5fb7eccf
tom-2810 <tom.poortman@hva.nl>
2022-12-15 15:55:08 +0100

    darkmode fix, stop scroll na openen hamburger menu

--- scripts/menu.js
+++ scripts/menu.js
@@ -1,3 +1,4 @@
 document.querySelector("#hamburger-icon").addEventListener("click", function() {
     document.querySelector("#navigation #top-nav").classList.toggle("open");
-})
+    document.body.classList.toggle("stop-scroll");
+});
