03f98d5baf53deb8fb9f1a5cb2674da0d0d25d7a
tom-2810 <tom.poortman@hva.nl>
2022-12-19 13:18:20 +0100

    localStorage theme mode

--- scripts/light-dark-mode.js
+++ scripts/light-dark-mode.js
@@ -1,7 +1,16 @@
 let modeSwitch = document.getElementById("mode").getElementsByTagName("input")[0];
+let savedTheme = localStorage.getItem("theme");
+
+if (savedTheme === "dark") {
+    modeSwitch.checked = true;
+    document.documentElement.setAttribute("data-theme", savedTheme);
+}
 
 modeSwitch.addEventListener('click', function () {
-    modeSwitch.checked ? 
-    document.documentElement.setAttribute("data-theme", "dark") : 
-    document.documentElement.setAttribute("data-theme", "");
+    modeSwitch.checked ? setTheme("dark") : setTheme("");
 });
+
+function setTheme(theme) {
+    document.documentElement.setAttribute("data-theme", theme);
+    localStorage.setItem("theme", theme);
+}
