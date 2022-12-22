2b1264cf3191f27b14404373f7b7ebd0b23799f9
tom-2810 <tom.poortman@hva.nl>
2022-12-22 22:47:40 +0100

    fancy 🍔 menu

--- scripts/menu.js
+++ scripts/menu.js
@@ -1,4 +1,13 @@
-document.querySelector("#hamburger-icon").addEventListener("click", function() {
+const hamburgerButton = document.querySelector("#navigation button");
+
+hamburgerButton.addEventListener("click", function() {
+    const isOpened = hamburgerButton.getAttribute('aria-expanded');
+    if (isOpened === "false") {
+        hamburgerButton.setAttribute("aria-expanded", "true");
+    } else {
+        hamburgerButton.setAttribute("aria-expanded", "false");
+    }
+
     document.querySelector("#navigation #top-nav").classList.toggle("open");
     document.body.classList.toggle("stop-scroll");
 });
